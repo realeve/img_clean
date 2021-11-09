1abb0b560dab106003595671fdaa32585d4252e8
realeve <realeve@qq.com>
2021-11-09 17:09:54 +0800

    detail update

--- src/pages/index/judge.tsx
+++ src/pages/index/judge.tsx
@@ -98,99 +98,95 @@ export default ({
   };
 
   return (
-    <Spin spinning={loading}>
-      <Row gutter={16} style={{ marginTop: 20 }}>
-        <Col span={12}>
-          图片默认大小(像素)：
-          <Radio.Group
-            defaultValue={defaultImageSize}
-            value={imgHeight}
-            buttonStyle="solid"
-            onChange={(e) => {
-              setImgHeight(e.target.value);
-            }}
-          >
-            {imgSize.map((item) => {
-              return (
-                <Radio.Button value={item} key={String(item)}>
-                  {item}
-                </Radio.Button>
-              );
-            })}
-          </Radio.Group>
-        </Col>
-        <Col span={12} style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <Button
-            size="large"
-            type="primary"
-            onClick={() => {
-              confirm({
-                onOk: submit,
-                title: '是否所有数据已经判废完成，确认提交？',
-                okText: '提交入库',
-                cancelText: '取消',
-              });
-            }}
-          >
-            确认提交
-          </Button>
-        </Col>
-        <Divider />
-        <Col span={12}>
-          <h1 style={{ textAlign: 'center' }}>
-            实废（{judgeData.fake.length}）
-          </h1>
-        </Col>
-        <Col span={12} style={{ borderLeft: '9px solid #888' }}>
-          <h1 style={{ textAlign: 'center' }}>
-            误废（{judgeData.normal.length}）
-          </h1>
-        </Col>
-        <Col span={12}>
-          <div className={styles.list}>
-            {judgeData.fake.map((id, i) => {
-              const item = data.find((item) => item.id === id) as IImageItem;
-              return (
-                <ImageItem
-                  item={item}
-                  key={id}
-                  onChange={() => {
-                    const fake = R.remove(i, 1, judgeData.fake);
-                    const normal = R.append(id, judgeData.normal);
-                    setJudgeData({
-                      fake,
-                      normal,
-                    });
-                  }}
-                  imgHeight={imgHeight}
-                />
-              );
-            })}
-          </div>
-        </Col>
-        <Col span={12} style={{ borderLeft: '9px solid #888' }}>
-          <div className={styles.list}>
-            {judgeData.normal.map((id, i) => {
-              const item = data.find((item) => item.id === id) as IImageItem;
-              return (
-                <ImageItem
-                  item={item}
-                  key={id}
-                  onChange={() => {
-                    const normal = R.remove(i, 1, judgeData.normal);
-                    const fake = R.append(id, judgeData.fake);
-                    setJudgeData({
-                      fake,
-                      normal,
-                    });
-                  }}
-                  imgHeight={imgHeight}
-                />
-              );
-            })}
-          </div>
-        </Col>
-      </Row>
-    </Spin>
+    <Row gutter={16} style={{ marginTop: 20 }}>
+      <Col span={12}>
+        图片默认大小(像素)：
+        <Radio.Group
+          defaultValue={defaultImageSize}
+          value={imgHeight}
+          buttonStyle="solid"
+          onChange={(e) => {
+            setImgHeight(e.target.value);
+          }}
+        >
+          {imgSize.map((item) => {
+            return (
+              <Radio.Button value={item} key={String(item)}>
+                {item}
+              </Radio.Button>
+            );
+          })}
+        </Radio.Group>
+      </Col>
+      <Col span={12} style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Button
+          size="large"
+          type="primary"
+          onClick={() => {
+            confirm({
+              onOk: submit,
+              title: '是否所有数据已经判废完成，确认提交？',
+              okText: '提交入库',
+              cancelText: '取消',
+            });
+          }}
+        >
+          确认提交
+        </Button>
+      </Col>
+      <Divider />
+      <Col span={12}>
+        <h1 style={{ textAlign: 'center' }}>实废（{judgeData.fake.length}）</h1>
+      </Col>
+      <Col span={12} style={{ borderLeft: '9px solid #888' }}>
+        <h1 style={{ textAlign: 'center' }}>
+          误废（{judgeData.normal.length}）
+        </h1>
+      </Col>
+      <Col span={12}>
+        <div className={styles.list}>
+          {judgeData.fake.map((id, i) => {
+            const item = data.find((item) => item.id === id) as IImageItem;
+            return (
+              <ImageItem
+                item={item}
+                key={id}
+                onChange={() => {
+                  const fake = R.remove(i, 1, judgeData.fake);
+                  const normal = R.append(id, judgeData.normal);
+                  setJudgeData({
+                    fake,
+                    normal,
+                  });
+                }}
+                imgHeight={imgHeight}
+              />
+            );
+          })}
+        </div>
+      </Col>
+      <Col span={12} style={{ borderLeft: '9px solid #888' }}>
+        <div className={styles.list}>
+          {judgeData.normal.map((id, i) => {
+            const item = data.find((item) => item.id === id) as IImageItem;
+            return (
+              <ImageItem
+                item={item}
+                key={id}
+                onChange={() => {
+                  const normal = R.remove(i, 1, judgeData.normal);
+                  const fake = R.append(id, judgeData.fake);
+                  setJudgeData({
+                    fake,
+                    normal,
+                  });
+                }}
+                imgHeight={imgHeight}
+              />
+            );
+          })}
+        </div>
+      </Col>
+    </Row>
   );
 };
