ad610c90d9757a319de2421f2ae1be9a814eb37b
realeve <realeve@qq.com>
2021-11-10 15:06:30 +0800

    更新编译配置

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,16 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  hash: true,
+  exportStatic: { htmlSuffix: false },
   // mfsu: {},
   routes,
+  targets: {
+    chrome: 70,
+    firefox: false,
+    safari: false,
+    edge: false,
+    ios: false,
+  },
   fastRefresh: {},
 });
