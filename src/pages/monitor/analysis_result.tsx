0262515157ab2d8da0d5ab9e1ecdea7861e9aeb0
李宾 <41192163@qq.com>
2022-02-16 08:38:10 +0800

    实物审核数据汇总

--- src/pages/monitor/analysis_result.tsx
+++ src/pages/monitor/analysis_result.tsx
@@ -4,10 +4,157 @@ import { ICartItem } from './db';
 import ResultPanel from './ResultPanel';
 import useFetch from '@/component/hooks/useFetch';
 import { DEV, IAxiosState } from '@/utils/axios';
-import { columns as tableColumns } from './index';
+import { imageSearchUrl } from '@/utils/setting';
 
 const columns = [
-  ...tableColumns,
+  {
+    title: '#',
+    dataIndex: 'idx',
+    key: 'idx',
+  },
+  {
+    title: '车号',
+    dataIndex: 'cart',
+    key: '车号',
+    render: (text, record: ICartItem) =>
+      record.id == '0' ? (
+        text
+      ) : (
+        <span>
+          <a target="_blank" href={imageSearchUrl + text}>
+            {text}
+          </a>
+        </span>
+      ),
+  },
+  {
+    title: '判废时间',
+    dataIndex: 'judge_date',
+    key: '判废时间',
+  },
+  {
+    title: '冠号',
+    key: '冠号',
+    dataIndex: 'head',
+  },
+  {
+    title: 'AI误废',
+    children: [
+      {
+        title: 'AI漏检',
+        children: [
+          {
+            title: <span style={{ color: 'red' }}>AI漏检</span>,
+            key: '审核实废',
+            dataIndex: 'leak_normal_fake_img',
+            render: (text, record) => (
+              <span style={{ color: record.id == 0 ? 'red' : 'unset' }}>
+                {Number(text).toFixed(1)}
+              </span>
+            ),
+          },
+          {
+            title: '人工误判',
+            key: '审核误判',
+            dataIndex: 'leak_normal_normal_img',
+            render: (text) => <span>{Number(text).toFixed(1)}</span>,
+          },
+          {
+            title: '合计',
+            key: '合计',
+            dataIndex: 'leak_normal_img',
+            render: (text) => <span>{Number(text).toFixed(1)}</span>,
+          },
+        ],
+      },
+      {
+        title: '共同检出误废',
+        key: '共同检出误废',
+        dataIndex: 'normal_img',
+        render: (text) => <span>{Number(text).toFixed(1)}</span>,
+      },
+    ],
+  },
+  {
+    title: 'AI实废',
+    children: [
+      {
+        title: 'AI误检',
+        children: [
+          {
+            title: <span style={{ color: 'red' }}>人工漏检</span>,
+            key: '审核实废',
+            dataIndex: 'err_fake_fake_img',
+            render: (text, record) => (
+              <span style={{ color: record.id == 0 ? 'red' : 'unset' }}>
+                {Number(text).toFixed(1)}
+              </span>
+            ),
+          },
+          {
+            title: 'AI误判',
+            key: '审核误判',
+            dataIndex: 'err_fake_normal_img',
+            render: (text) => <span>{Number(text).toFixed(1)}</span>,
+          },
+          {
+            title: '合计',
+            key: '合计',
+            dataIndex: 'err_fake_img',
+            render: (text) => <span>{Number(text).toFixed(1)}</span>,
+          },
+        ],
+      },
+      {
+        title: '共同检出实废',
+        key: '共同检出实废',
+        dataIndex: 'fake_img',
+        render: (text) => <span>{Number(text).toFixed(1)}</span>,
+      },
+    ],
+  },
+  {
+    title: '图片总数',
+    key: '图片总数',
+    dataIndex: 'total_img',
+    render: (text) => <span>{Number(text).toFixed(0)}</span>,
+  },
+  {
+    title: '准确率',
+    children: [
+      {
+        title: 'AI准确率',
+        children: [
+          {
+            title: '图核判废',
+            key: '图核判废',
+            dataIndex: 'acc',
+            render: (text) => <span>{Number(text).toFixed(2)}%</span>,
+          },
+          {
+            title: <span style={{ color: 'red' }}>实物审核</span>,
+            key: '实物审核',
+            dataIndex: 'acc_fix',
+            render: (text, record) => (
+              <span style={{ color: record.id == 0 ? 'red' : 'unset' }}>
+                {Number(text).toFixed(2)}%
+              </span>
+            ),
+          },
+        ],
+      },
+      {
+        title: <span style={{ color: 'red' }}>人工准确率</span>,
+        key: '人工判废',
+        dataIndex: 'accHuman',
+        render: (text, record) => (
+          <span style={{ color: record.id == 0 ? 'red' : 'unset' }}>
+            {Number(text).toFixed(2)}%
+          </span>
+        ),
+      },
+    ],
+  },
   {
     title: '* 不计废',
     key: 'ignore_img',
@@ -26,6 +173,12 @@ export default () => {
         ...item,
         idx: i || '',
         acc: Number(item.acc),
+        accHuman:
+          100 -
+          ((Number(item.leak_normal_normal_img) +
+            Number(item.err_fake_fake_img)) *
+            100) /
+            Number(item.total_img),
       })),
   });
 
@@ -45,7 +198,7 @@ export default () => {
       <ResultPanel show={show} setShow={setShow} cartinfo={cartinfo} />
       <Table
         dataSource={data}
-        loading={loading}
+        // loading={loading}
         pagination={{
           pageSize: 12,
         }}
